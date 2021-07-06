9568fd8d36c0b3c3172ff7cc53b0dde0cec455a1
andrentv <comercial@vaccaros.com.br>
2021-07-06 15:53:45 -0300

    POST rota segura

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+// use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -9,14 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class MessagesController extends AbstractController
 {
     /**
-     * @Route("/messages/{id}/like/{direction}")
+     * @Route("/messages/{id<\d+>}/like/{direction<like|dislike>}", methods="POST")
      */
 
+    // public function messagesLike($id, $direction,LoggerInterface $logger)
     public function messagesLike($id, $direction)
     {
         if ($direction === 'like'){
+            // $logger->info('Houve um Like!');            
             $currentLike = rand(80,200);
         } else {
+            // $logger->info('Houve um Dislike!');
             $currentLike = rand(10,20);
         }
 
